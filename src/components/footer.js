7eec77d0c7da6d288c8a66889de1c08bd19317a7
Lucio Afonso <lucioafonso@icloud.com>
2021-01-23 19:17:18 +0100

    Updated footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -8,18 +8,10 @@ const Footer = ({ author }) => {
       <div>
         <SocialLinks styleClass="footer-links" />
         <h4>
-          &copy; {new Date().getFullYear()}. <strong>{author}</strong>{" "}
+          Copyright &copy; {new Date().getFullYear()}. <strong>{author}</strong>
+          {" - "}
           <span>WebDev</span>.
         </h4>
-        <p>
-          All rights reserved. Built with{" "}
-          <a href="https://gatsbyjs.com">
-            <span style={{ color: "white", textDecoration: "underline" }}>
-              GatsbyJS
-            </span>
-          </a>
-          .
-        </p>
       </div>
     </footer>
   )
