76f549a2dcf64ca0a5636c41d3f15f375ef72dfa
Lucio Afonso <lucioafonso@icloud.com>
2021-01-24 16:52:34 +0100

    Added about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -5,10 +5,52 @@ import Image from "gatsby-image"
 import Title from "../components/title"
 import Layout from "../components/layout"
 
-// ...GatsbyImageSharpFluid
-
-const AboutPage = () => {
-  return <h2>about page</h2>
+const AboutPage = ({
+  data: {
+    strapi: { about },
+  },
+}) => {
+  const { title, stack, image, info } = about[0]
+  return (
+    <Layout>
+      <section className="about-page">
+        <div className="section-center about-center">
+          {image && <Image fluid={image.img.fluid} className="about-img" />}
+          <article className="about-text">
+            <Title title={title} />
+            <p>{info}</p>
+            <div className="about-stack">
+              {stack.map(item => (
+                <span key={item.id}>{item.name}</span>
+              ))}
+            </div>
+          </article>
+        </div>
+      </section>
+    </Layout>
+  )
 }
+
+export const query = graphql`
+  {
+    strapi: allStrapiAbout {
+      about: nodes {
+        image {
+          img: childImageSharp {
+            fluid {
+              ...GatsbyImageSharpFluid
+            }
+          }
+        }
+        title
+        stack {
+          id
+          name
+        }
+        info
+      }
+    }
+  }
+`
 
 export default AboutPage
