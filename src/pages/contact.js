30a115db91d1fcb8ef0299d74590e0fac5f4e194
Lucio Afonso <la@euro-linux.com>
2021-02-05 21:00:39 +0100

    WIP: contact page

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -3,7 +3,44 @@ import React from "react"
 import Layout from "../components/layout"
 
 const ContactPage = () => {
-  return <h2>contact page</h2>
+  return (
+    <Layout>
+      <section className="contact-page">
+        <article className="contact-form">
+          <h3>Get in touch</h3>
+          <form method="post" rel="noreferrer noopener" target="_blank">
+            <div className="form-group">
+              <input
+                type="text"
+                name="name"
+                placeholder="name"
+                className="form-control"
+                required
+              />
+              <input
+                type="email"
+                name="email"
+                placeholder="your@email.com"
+                className="form-control"
+                required
+              />
+              <textarea
+                name="message"
+                name="message"
+                rows="7"
+                placeholder="your message goes here"
+                className="form-control"
+                required
+              ></textarea>
+            </div>
+            <button type="submit" className="submit-btn btn">
+              Submit
+            </button>
+          </form>
+        </article>
+      </section>
+    </Layout>
+  )
 }
 
 export default ContactPage
