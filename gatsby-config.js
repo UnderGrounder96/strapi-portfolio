4a56b913c2da2826a8386c2a86d7e9f425ededc7
Lucio Afonso <36516559+UnderGrounder96@users.noreply.github.com>
2021-02-07 12:22:49 +0100

    Ready for production (#18)
    * Updated contact page
    
    * Removed blog related content
    
    * Ready for production
    
    Co-authored-by: Lucio Afonso <la@euro-linux.com>

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,7 +10,8 @@ module.exports = {
     description: `This WebDev Portfolio Site that uses Strapi and GatsbyJS.`,
     author: `Lucio Afonso`,
     twitterUsername: `@UnderGrounder96`,
-    siteUrl: "http://127.0.0.1:9000",
+    image: `/twitter-card.png`,
+    siteUrl: "https://lucio-afonso-portfolio.netlify.app",
     social: [
       {
         name: `linkedin`,
@@ -18,7 +19,7 @@ module.exports = {
       },
       {
         name: `github`,
-        url: `https://bit.ly/2LRZ0wn`,
+        url: `https://github.com/UnderGrounder96`,
       },
     ],
   },
